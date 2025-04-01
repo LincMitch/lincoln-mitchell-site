e61f88e0259044b84d0038180b1427f247f5a888
LincAtPenCS <lincoln.mitchell@pencs.com.au>
2025-04-01 15:30:12 +1100

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe"
 
 // Initialize Stripe with your secret key
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2023-10-16",
+  apiVersion: "2025-02-24.acacia",
 })
 
 // Create a payment intent
